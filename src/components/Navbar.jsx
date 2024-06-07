5995ec7e03656826eb8f0a11c4129c5ffef4b519
for_lack_of_a_better_name <vivekkr174@gmail.com>
2024-06-07 15:03:12 +0530

    project completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div>
         <nav class="navbar navbar-expand-lg bg-warning ">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Student's App</a>
+    <Link class="navbar-brand" to="#">Student's App</Link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/view">View Students</a>
+          <Link class="nav-link active" aria-current="page" to="/view">View Students</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/">Add Student</a>
+          <Link class="nav-link" to="/">Add Student</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/search">Search Student</a>
+          <Link class="nav-link" to="/search">Search Student</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/delete">Delete Student</a>
+          <Link class="nav-link" to="/delete">Delete Student</Link>
         </li>
       </ul>
     </div>
