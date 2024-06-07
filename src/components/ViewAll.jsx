aaf372903e83a15ebe5b704001acec289fce62f3
for_lack_of_a_better_name <vivekkr174@gmail.com>
2024-06-07 14:35:26 +0530

    project completed

---
+++ src/components/ViewAll.jsx
@@ -0,0 +1,84 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const ViewAll = () => {
+    return (
+        <div>
+            <Navbar/>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-4">
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://i1.sndcdn.com/artworks-oyyH7Y75Wyacu0FU-MnZWDw-t500x500.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <p class="card-text">StickMon</p>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-3 col-xxl-3">
+                            <div class="card" >
+                                    <img src="https://i1.sndcdn.com/artworks-oyyH7Y75Wyacu0FU-MnZWDw-t500x500.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <p class="card-text">StickMon</p>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-3 col-xxl-3">
+                            <div class="card" >
+                                    <img src="https://i1.sndcdn.com/artworks-oyyH7Y75Wyacu0FU-MnZWDw-t500x500.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <p class="card-text">StickMon</p>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-3 col-xxl-3">
+                            <div class="card" >
+                                    <img src="https://i1.sndcdn.com/artworks-oyyH7Y75Wyacu0FU-MnZWDw-t500x500.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <p class="card-text">StickMon</p>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-3 col-xxl-3">
+                            <div class="card" >
+                                    <img src="https://i1.sndcdn.com/artworks-oyyH7Y75Wyacu0FU-MnZWDw-t500x500.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <p class="card-text">StickMon</p>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-3 col-xxl-3">
+                            <div class="card" >
+                                    <img src="https://i1.sndcdn.com/artworks-oyyH7Y75Wyacu0FU-MnZWDw-t500x500.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <p class="card-text">StickMon</p>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-3 col-xxl-3">
+                            <div class="card" >
+                                    <img src="https://i1.sndcdn.com/artworks-oyyH7Y75Wyacu0FU-MnZWDw-t500x500.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <p class="card-text">StickMon</p>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-3 col-xxl-3">
+                            <div class="card" >
+                                    <img src="https://i1.sndcdn.com/artworks-oyyH7Y75Wyacu0FU-MnZWDw-t500x500.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <p class="card-text">StickMon</p>
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ViewAll
